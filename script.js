16171cdee0a88793701c577f20b14598db6dfe1f
Svetlana Zaretkaya <svetlanazaretskaya@yandex.ru>
2024-08-24 20:02:07 +0300

    done

---
+++ script.js
@@ -0,0 +1,9 @@
+const image = document.getElementById(`imageID`);
+
+function addImageNext() {
+    image.src ="https://img.gazeta.ru/files3/34/14989034/59051691-10915089-image-m-34_165-pic_32ratio_1200x800-1200x800-85861.jpg"
+}
+
+function addImagePrevious() {
+    image.src="https://www.vetaclinic.ru/wp-content/uploads/2020/10/cat.jpg"
+}
